69b24af2167bf4248387799e24ca335ad4688898
moslim <moslim@DESKTOP-CGAS3NR>
2025-05-07 14:55:42 +0200

    Product Controller Initial_Create

---
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositries;
+
+namespace Talabat.APIs.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly IGenericReopositiory<Product> _genericReopositiory;
+
+        public ProductsController( IGenericReopositiory<Product> genericReopositiory  )
+        {
+            _genericReopositiory = genericReopositiory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult< IEnumerable<Product> >> GetAllProducts()
+        {
+            var products = await _genericReopositiory.GetAllAsync(); 
+            return Ok(products);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var Product = await _genericReopositiory.GetByIdAsync(id);
+            return Ok(Product);
+        }
+
+    }
+}
--- Talabat.APIs/Startup.cs
+++ Talabat.APIs/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Talabat.Core.Repositries;
+using Talabat.Repository;
 using Talabat.Repository.Data;
 
 namespace Talabat.APIs
@@ -42,6 +44,8 @@ namespace Talabat.APIs
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             }
             );
+
+            services.AddScoped(typeof(IGenericReopositiory<>), typeof(GenericRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
